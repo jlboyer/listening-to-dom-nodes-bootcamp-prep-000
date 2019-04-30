9513730f2eb7dfa50719d4f16438ad64b1de1ef6
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-04-30 03:06:02 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ main.addEventListener('click', function(event) {
   });
 
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
-  if (e.key === "g") {
+  if (e.key === "d") {
     return e.preventDefault()
   } else {
     console.log(e.key)
@@ -18,7 +18,7 @@ input.addEventListener('keydown', function(e) {
 });
 
 let divs = document.querySelectorAll('div');
- 
+
 /* function bubble(e) {
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
@@ -39,10 +39,10 @@ for (let i = 0; i < divs.length; i++) {
 function bubble(e) {
   // stop! that! propagation!
   e.stopPropagation();
- 
+
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }
