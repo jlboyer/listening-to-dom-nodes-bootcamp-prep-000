61ceacac1399470d221a68d8da380301f37c9c73
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-04-30 03:07:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ main.addEventListener('click', function(event) {
 const input = document.querySelector('input');
 
 input.addEventListener('keydown', function(e) {
-  if (e.key === "d") {
+  if (e.key === "g") {
     return e.preventDefault()
   } else {
     console.log(e.key)
